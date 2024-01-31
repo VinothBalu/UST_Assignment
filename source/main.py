940f75928ab82a14a1436568d1ef12573cb73085
Vino <vinothbalu3007m@gmail.com>
2024-01-31 11:18:29 +0530

    main.py file created

--- source/main.py
+++ source/main.py
@@ -1,7 +1,7 @@
 import threading
 
 from source.DivisibleByFive import divisible_by_five
-from source.Palindrome_check import Integer
+from source.Palindrome_check import Singleton,Integer
 
 
 def main():
@@ -13,3 +13,12 @@ def main():
     result_program1 = divisible_by_five(binary_numbers)
     print(f"Program 1 Output: {','.join(result_program1)}")
 
+    with open('../Input_Data/Palindrome_Check.txt', 'r') as file:
+        palindrome_input = file.read()
+    n = Integer(int(palindrome_input))
+    print(f"Program 2 Output: {n.value} is {'a palindrome' if n.is_palindrome() else 'not a palindrome'}")
+
+
+
+if __name__ == "__main__":
+    main()
